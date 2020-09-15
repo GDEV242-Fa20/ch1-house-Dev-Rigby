78d7a2660a48733d4d32dd290fc3fdf8b6caf9ed
Dev-Rigby <71235797+Dev-Rigby@users.noreply.github.com>
2020-09-14 20:50:19 -0400

    Project 1 Commit
    First attmpt commiting project 1 to github

--- Picture.java
+++ Picture.java
@@ -1,20 +1,21 @@
+
 /**
- * This class represents a simple picture. You can draw the picture using
- * the draw method. But wait, there's more: being an electronic picture, it
- * can be changed. You can set it to black-and-white display and back to
- * colors (only after it's been drawn, of course).
- *
- * This class was written as an early example for teaching Java with BlueJ.
+ * A class to display a picture of a monitor and a man that nac be put into
+ * black and white and then changed back to color.
  * 
- * @author  Michael Kšlling and David J. Barnes
- * @version 2016.02.29
+ * @author Brian McMahon G00274188
+ * @version 1.0.0
  */
 public class Picture
 {
-    private Square wall;
-    private Square window;
-    private Triangle roof;
-    private Circle sun;
+    // instance variables - replace the example below with your own
+    private Square monitorOut;
+    private Square monitorIn;
+    private Square monitorStand;
+    private Square monitorStandTwo;
+    private Triangle logo;
+    private Circle powerButton;
+    private Person man;
     private boolean drawn;
 
     /**
@@ -22,64 +23,79 @@ public class Picture
      */
     public Picture()
     {
-        wall = new Square();
-        window = new Square();
-        roof = new Triangle();  
-        sun = new Circle();
+        monitorOut = new Square();
+        monitorIn = new Square();
+        monitorStand = new Square();
+        monitorStandTwo = new Square();
+        logo = new Triangle();
+        powerButton = new Circle();
+        man = new Person();
         drawn = false;
     }
 
     /**
-     * Draw this picture.
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
      */
     public void draw()
     {
-        if(!drawn) {
-            wall.moveHorizontal(-140);
-            wall.moveVertical(20);
-            wall.changeSize(120);
-            wall.changeColor("blue");
-            wall.makeVisible();
+        if(!drawn)
+        {
+            monitorOut.changeSize(150);
+            monitorOut.changeColor("black");
+            monitorOut.moveVertical(-20);
+            monitorOut.moveHorizontal(-100);
+            monitorOut.makeVisible();
             
-            window.changeColor("black");
-            window.moveHorizontal(-120);
-            window.moveVertical(40);
-            window.changeSize(40);
-            window.makeVisible();
-    
-            roof.changeSize(60, 180);
-            roof.moveHorizontal(20);
-            roof.moveVertical(-60);
-            roof.makeVisible();
-    
-            sun.changeColor("yellow");
-            sun.moveHorizontal(100);
-            sun.moveVertical(-40);
-            sun.changeSize(80);
-            sun.makeVisible();
-            drawn = true;
+            monitorIn.changeSize(130);
+            monitorIn.changeColor("blue");
+            monitorIn.moveVertical(-10);
+            monitorIn.moveHorizontal(-90);
+            monitorIn.makeVisible();
+            
+            monitorStand.changeSize(50);
+            monitorStand.changeColor("black");
+            monitorStand.moveVertical(120);
+            monitorStand.moveHorizontal(-50);
+            monitorStand.makeVisible();
+            
+            monitorStandTwo.changeSize(100);
+            monitorStandTwo.changeColor("black");
+            monitorStandTwo.moveVertical(170);
+            monitorStandTwo.moveHorizontal(-75);
+            monitorStandTwo.makeVisible();
+            
+            logo.changeSize(20,10);
+            logo.changeColor("green");
+            logo.moveVertical(120);
+            logo.moveHorizontal(75);
+            logo.makeVisible();
+            
+            powerButton.changeSize(5);
+            powerButton.changeColor("red");
+            powerButton.moveVertical(152);
+            powerButton.moveHorizontal(-15);
+            powerButton.makeVisible();
+            
+            man.changeSize(100, 60);
+            man.changeColor("black");
+            man.moveVertical(40);
+            man.moveHorizontal(-200);
+            man.makeVisible();
         }
     }
-
-    /**
-     * Change this picture to black/white display
-     */
     public void setBlackAndWhite()
     {
-        wall.changeColor("black");
-        window.changeColor("white");
-        roof.changeColor("black");
-        sun.changeColor("black");
+        monitorIn.changeColor("white");
+        logo.changeColor("white");
+        powerButton.changeColor("white");
     }
-
-    /**
-     * Change this picture to use color display
-     */
     public void setColor()
     {
-        wall.changeColor("red");
-        window.changeColor("black");
-        roof.changeColor("green");
-        sun.changeColor("yellow");
+        monitorIn.changeColor("blue");
+        logo.changeColor("green");
+        powerButton.changeColor("red");
     }
 }
